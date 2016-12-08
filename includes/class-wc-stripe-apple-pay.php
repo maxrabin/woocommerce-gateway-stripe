b907133cd55290f75dc5eac92ee76ae00b3f1e3e
Max Rabin <rabin.max@gmail.com>
2016-12-08 14:23:37 +0200

    Uppercase Country code returned from Apple Pay
    Apple Pay / Stripe return country codes as lowercase, and Woocommerce expects them to be uppercase.

--- includes/class-wc-stripe-apple-pay.php
+++ includes/class-wc-stripe-apple-pay.php
@@ -412,7 +412,7 @@ class WC_Stripe_Apple_Pay extends WC_Gateway_Stripe {
 			$billing_address['last_name']  = $lastname;
 			$billing_address['email']      = $data['shippingContact']['emailAddress'];
 			$billing_address['phone']      = $data['shippingContact']['phoneNumber'];
-			$billing_address['country']    = $data['token']['card']['country'];
+			$billing_address['country']    = strtoupper($data['token']['card']['country']);
 			$billing_address['address_1']  = $data['token']['card']['address_line1'];
 			$billing_address['address_2']  = $data['token']['card']['address_line2'];
 			$billing_address['city']       = $data['token']['card']['address_city'];
@@ -427,7 +427,7 @@ class WC_Stripe_Apple_Pay extends WC_Gateway_Stripe {
 			$shipping_address['last_name']  = $data['shippingContact']['familyName'];
 			$shipping_address['email']      = $data['shippingContact']['emailAddress'];
 			$shipping_address['phone']      = $data['shippingContact']['phoneNumber'];
-			$shipping_address['country']    = $data['shippingContact']['countryCode'];
+			$shipping_address['country']    = strtoupper($data['shippingContact']['countryCode']);
 			$shipping_address['address_1']  = $data['shippingContact']['addressLines'][0];
 			$shipping_address['city']       = $data['shippingContact']['locality'];
 			$shipping_address['state']      = $data['shippingContact']['administrativeArea'];
@@ -437,7 +437,7 @@ class WC_Stripe_Apple_Pay extends WC_Gateway_Stripe {
 			$shipping_address['last_name']  = $lastname;
 			$shipping_address['email']      = $data['shippingContact']['emailAddress'];
 			$shipping_address['phone']      = $data['shippingContact']['phoneNumber'];
-			$shipping_address['country']    = $data['token']['card']['country'];
+			$shipping_address['country']    = strtoupper($data['token']['card']['country']);
 			$shipping_address['address_1']  = $data['token']['card']['address_line1'];
 			$shipping_address['address_2']  = $data['token']['card']['address_line2'];
 			$shipping_address['city']       = $data['token']['card']['address_city'];
